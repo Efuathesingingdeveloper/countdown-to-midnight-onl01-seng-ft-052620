79272e677fca62c2aa87284dfec5abe6c19557f9
Georgette Class-Peters <georgycp@udel.edu>
2020-06-03 00:01:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,6 @@
-#write your code here
-
-def countdown
-end
+#write your code 
+def countdown_with_sleep
+  return(HAPPY NEW YEAR !)
+  runtime = Benchmark.measure { countdown_with_sleep(5) }
+  end 
+end 
